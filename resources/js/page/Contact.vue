958b98f428a9e6389be59b0bcc2a06e9bb252a5c
Guz117 <cristian.guzman117@gmail.com>
2022-03-14 17:39:45 +0100

    test invio mail

--- resources/js/page/Contact.vue
+++ resources/js/page/Contact.vue
@@ -1,20 +1,30 @@
 <template>
     
     <div class="container">
-        
-        <form>
+        <div v-if="success" class="alert alert-success" role="alert">
+            Mail inviata con successo!
+        </div>
+        <form @submit.prevent="sendForm">
             <div class="form-group">
                 <label for="name">Nome</label>
                 <input v-model="name" type="text" class="form-control" id="name" name="name">
-                
+                <p class="alert alert-danger" v-for="(error, index) in errors.name" :key="index">
+                    {{ error }}
+                </p>
             </div>
             <div class="form-group">
                 <label for="email">Email</label>
                 <input v-model="email" type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
+                <p class="alert alert-danger" v-for="(error, index) in errors.name" :key="index">
+                    {{ error }}
+                </p>
             </div>
             <div class="form-group">
                 <label for="message">Messaggio</label>
                 <textarea v-model="message" class="form-control" id="message" name="message" rows="3"></textarea>
+                <p class="alert alert-danger" v-for="(error, index) in errors.name" :key="index">
+                    {{ error }}
+                </p>
             </div>
 
             <button type="submit" class="btn btn-primary">{{ sending ? "Sto inviando..." : "Invia" }}</button>
@@ -25,7 +35,45 @@
 
 <script>
 export default {
-    name: 'Contact'
+    name: 'Contact',
+    data() {
+        return {
+            name: null,
+            email: null,
+            message: null,
+            success: false,
+            sending: false,
+            errors: {}
+        }
+    },
+    methods: {
+        sendForm(){
+            this.sending = true
+            axios.post('/api/contacts',
+            {
+                'name': this.name,
+                'email': this.email,
+                'message': this.message,
+            })
+            .then(response=>{
+                console.log(response.data);
+                if (!response.data.succes){
+                    this.success = false;
+                    this.errors = response.data.errors;
+                } else {
+                    this.success = true;
+                    this.errors = {}
+                }
+
+                this.sending = false
+            })
+            .catch(error=> {
+                console.log(error.response.data);
+                this.sending = false
+            })
+        }
+
+    }
 }
 </script>
 
